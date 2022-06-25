b222c553a792990212c9f69ad2f6d38c85b8b4df
Pradeesh <pradeeshnarayan@gmail.com>
2022-06-25 22:07:29 +0530

    Adding new menus Changing department - Display name

--- TaskManager.Model/Department.cs
+++ TaskManager.Model/Department.cs
@@ -13,6 +13,7 @@ namespace TaskManager.Model
         public int Id { get; set; }
 
         [Required(ErrorMessage = "Department Name is required..")]
+        [Display(Name = "Department")]
         public string DepartmentName { get; set; }
 
         public int CreatedBy { get; set; }
