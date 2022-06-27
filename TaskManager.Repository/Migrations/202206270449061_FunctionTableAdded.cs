dca58dfe520f0a8c0e6f9d6812c0737ef8b4bb9a
Remya T <rthiyyath@omnex.com>
2022-06-27 11:15:23 +0530

    Added function table migration file.

---
+++ TaskManager.Repository/Migrations/202206270449061_FunctionTableAdded.cs
@@ -0,0 +1,32 @@
+﻿namespace TaskManager.Repository.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class FunctionTableAdded : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Functions",
+                c => new
+                    {
+                        Id = c.Int(nullable: false),
+                        FunctionName = c.String(nullable: false),
+                        DepartmentID = c.Int(nullable: false),
+                        CreatedBy = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Departments", t => t.Id)
+                .Index(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Functions", "Id", "dbo.Departments");
+            DropIndex("dbo.Functions", new[] { "Id" });
+            DropTable("dbo.Functions");
+        }
+    }
+}
